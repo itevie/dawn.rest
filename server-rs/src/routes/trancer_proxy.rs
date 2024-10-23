c77947b4aa474290fa012fbc1d56edf23d28453b
Isabella <izzyplays07@gmail.com>
2024-10-23 23:25:40 +0100

    +1

--- server-rs/src/routes/trancer_proxy.rs
+++ server-rs/src/routes/trancer_proxy.rs
@@ -1,13 +1,39 @@
 use reqwest::Client;
-use rocket::{http::Status, response::content::RawJson};
+use rocket::{
+    http::Status,
+    request::{FromRequest, Outcome},
+    response::content::RawJson,
+    Request,
+};
+
+// Create a custom guard to extract the token
+#[derive(Debug)]
+pub struct TokenGuard(String);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for TokenGuard {
+    type Error = ();
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        if let Some(h) = request.headers().get_one("Authorization") {
+            Outcome::Success(TokenGuard(h.to_string()))
+        } else {
+            Outcome::Error((Status::Unauthorized, ()))
+        }
+    }
+}
 
 #[get("/data/<data_type>")]
-pub async fn get_from_trancer(data_type: &str) -> Result<RawJson<String>, Status> {
+pub async fn get_from_trancer(
+    data_type: &str,
+    header: TokenGuard,
+) -> Result<RawJson<String>, Status> {
     let client = Client::new();
     let url = format!("http://localhost:8080/data/{}", data_type);
 
     let response = client
         .get(&url)
+        .header("Authorization", header.0)
         .send()
         .await
         .map_err(|_| Status::InternalServerError)?;
