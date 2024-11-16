820214c277a932ee74dadb7219f6dfcfc4b8ce0a
Isabella <izzyplays07@gmail.com>
2024-11-16 00:42:38 +0000

    graph

--- src/pages/Trancer/TrancerPage.tsx
+++ src/pages/Trancer/TrancerPage.tsx
@@ -1,16 +1,25 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 import Link from "../../dawn-ui/components/Link";
 import axios from "axios";
-import { Chart as ChartJS, registerables } from "chart.js";
+import {
+  Chart as ChartJS,
+  registerables,
+  ChartDataset,
+  ChartConfiguration,
+} from "chart.js";
 import { Bar, Line } from "react-chartjs-2";
-import { showErrorAlert } from "../../dawn-ui/components/AlertManager";
+import {
+  showErrorAlert,
+  showLoadingAlert,
+} from "../../dawn-ui/components/AlertManager";
 import Row from "../../dawn-ui/components/Row";
 import Words from "../../dawn-ui/components/Words";
 import Container from "../../dawn-ui/components/Container";
 import DawnPage from "../../components/DawnPage";
 import Column from "../../dawn-ui/components/Column";
 import { ArrayElement } from "../../dawn-ui/util";
+import MultiSelect from "../../dawn-ui/components/MultiSelect";
 ChartJS.register(...registerables);
 
 interface TrancerData {
@@ -42,6 +51,14 @@ interface TrancerData {
     amount: number;
   }[];
   quotes?: Quote[];
+  moneyTransactions: { [key: string]: Transaction[] };
+}
+
+interface Transaction {
+  id: number;
+  user_id: string;
+  balance: number;
+  added_at: number;
 }
 
 interface Quote {
@@ -70,7 +87,7 @@ const timeFilters = {
 
 export default function TrancerPage() {
   const [page, setPage] = useState<string>("about");
-  const [data, setData] = useState<TrancerData>({});
+  const [data, setData] = useState<TrancerData>({ moneyTransactions: {} });
   const [timeFilter, setTimeFilter] = useState<{
     [key: string]: keyof typeof timeFilters;
   }>({ messages: "month", memberCount: "day" });
@@ -78,6 +95,10 @@ export default function TrancerPage() {
     null
   );
   const [randomQuote, setRandomQuote] = useState<Quote | null>(null);
+  const [selectedUserData, setSelectedUserData] = useState<string[]>([]);
+  const [mtChartData, setMtChartData] = useState<ChartConfiguration | null>(
+    null
+  );
 
   useEffect(() => {
     const hashtag = window.location.hash;
@@ -135,6 +156,117 @@ export default function TrancerPage() {
     } catch (e) {
       showErrorAlert(
         `Failed to load ${type}: Are you missing the auth code? Run .site in Trancy Twilight.`
+      );
+    }
+  }
+
+  function reloadMT(ddd: { [key: string]: Transaction[] }, ids: string[]) {
+    let idss = ids || selectedUserData;
+    let collected: Transaction[] = [];
+    for (const i in ddd)
+      if (idss.includes(data.usernames?.[i] ?? ""))
+        collected = collected.concat(ddd[i]);
+    collected = collected.sort((a, b) => a.id - b.id);
+
+    if (collected.length === 0) {
+      setMtChartData(null);
+      return;
+    }
+
+    const oldPre = new Date(collected[0].added_at);
+    const newPre = new Date(collected[collected.length - 1].added_at);
+    oldPre.setSeconds(0);
+    oldPre.setMinutes(0);
+    oldPre.setHours(0);
+    newPre.setSeconds(0);
+    newPre.setMinutes(0);
+    newPre.setHours(0);
+
+    const oldest = oldPre.getTime();
+    const newest = newPre.getTime();
+
+    const userTimes: { [key: string]: Transaction[] } = {};
+    for (const t of collected) {
+      if (!userTimes[t.user_id]) userTimes[t.user_id] = [];
+      let date = new Date(t.added_at);
+      date.setSeconds(0);
+      date.setMinutes(0);
+      date.setHours(0);
+      t.added_at = date.getTime();
+      userTimes[t.user_id].push(t);
+    }
+
+    const dd: { [key: string]: number[] } = {};
+    const labels: string[] = [];
+
+    for (let time = oldest; time < newest; time += 60000 * 60 * 60) {
+      labels.push(new Date(time).toLocaleString());
+      for (const user in userTimes) {
+        let gotData: number | null = null;
+        let prevData: number | null = null;
+
+        for (const t of userTimes[user]) {
+          if (t.added_at === time) {
+            gotData = t.balance;
+            break;
+          } else if (t.added_at < time) prevData = t.balance;
+        }
+
+        let d = gotData ?? prevData ?? 0;
+        if (!dd[user]) dd[user] = [];
+        dd[user].push(d);
+      }
+    }
+
+    const chartData: ChartDataset[] = [];
+    const colors =
+      "FF0000,00FF00,0000FF,FFFF00,00FFFF,FF00FF,BDB5D5,FFB6C1,AA336A,CF9FFF,00A36C,CC5500,C9CC3F,FF00FF,F28C28"
+        .split(",")
+        .map((x) => `#${x}`);
+    let index = 0;
+
+    for (const d in dd) {
+      chartData.push({
+        label: data.usernames?.[d],
+        data: dd[d],
+        borderColor: colors[index],
+      });
+      index++;
+    }
+    setMtChartData({
+      type: "line",
+      data: {
+        labels,
+        datasets: chartData,
+      },
+    });
+  }
+
+  async function loadMoneyTransaction(id: string, idss: string[]) {
+    try {
+      const responseData = await axios.get(
+        `${baseDataURL}/money_transactions-${id}`,
+        {
+          headers: {
+            Authorization: window.localStorage.getItem("trancer_auth"),
+          },
+        }
+      );
+      let json = responseData.data.data as Transaction[];
+
+      setData((old) => {
+        const n = {
+          ...old,
+          moneyTransactions: { ...old.moneyTransactions, [id]: json },
+        };
+
+        reloadMT(n.moneyTransactions, idss);
+
+        return n;
+      });
+    } catch (e) {
+      showErrorAlert(
+        `Failed to load money transactions for ${id}: Are you missing the auth code? Run .site in Trancy Twilight.`
       );
     }
   }
@@ -199,7 +331,44 @@ export default function TrancerPage() {
           </Container>
         ),
 
-        "user-data": <></>,
+        "user-data": (
+          <>
+            <p>Select the users you'd like to see in the graph</p>
+            <MultiSelect
+              onChange={async (e) => {
+                setSelectedUserData(e);
+                for (const i of e) {
+                  let id = Object.values(data.usernames ?? {}).findIndex(
+                    (x) => x === i
+                  );
+                  await loadMoneyTransaction(
+                    Object.keys(data.usernames ?? {})[id || "0"] || "0",
+                    e
+                  );
+                }
+                reloadMT(data.moneyTransactions, e);
+              }}
+              updateSelectedKey={0}
+              selected={selectedUserData}
+              elements={Object.values(data.usernames ?? {}).sort()}
+            />
+            <Container
+              title="Money Overtime"
+              style={{ marginTop: "20px", height: "80vh" }}
+            >
+              {mtChartData && (
+                <Line
+                  options={{ responsive: true, maintainAspectRatio: false }}
+                  style={{ width: "100%" }}
+                  data={{
+                    labels: mtChartData.data.labels,
+                    datasets: mtChartData.data.datasets as any,
+                  }}
+                />
+              )}
+            </Container>
+          </>
+        ),
 
         leaderboards: (
           <Column>
@@ -432,15 +601,14 @@ export default function TrancerPage() {
 }
 
 function convertTimes(
-  data: TrancerData["messages"],
+  data: any,
   regex: RegExp,
-  key: "amount",
+  key: "amount" | "balance",
   noIncrease: boolean = false
 ) {
   if (!data) return [];
 
   let times: { [key: string]: number } = {};
-  console.log(data);
   for (const d of data) {
     let time = d.time.match(regex);
     if (!time) continue;
